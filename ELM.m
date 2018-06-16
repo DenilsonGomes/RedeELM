03822ab739afba8df5d4344def3d0f5a24fc3ab4
Denilson Gomes <denilsongomesdg97@gmail.com>
2018-06-16 12:20:38 -0300

    Codigo ajustado

--- ELM.m
+++ ELM.m
@@ -3,23 +3,19 @@
 %Controle de Sistemas Dinamicos - Prof. Dr. Vandilberto
 %Programa para plotar as duas classes (cores diferentes) e plotar a
 %superficie de separação
-
 clc
+clf
 clear
 load twomoons.dat; %Carrega a base de dados
-
 X1 = twomoons(:,1); %X1 recebe o primeiro atributo
 X2 = twomoons(:,2); %X2 recebe o segundo atributo
-
 plot(X1(1:501),X2(1:501),'or'); %plota os elementos da primeira classe
 hold on; %segura o grafico
 plot(X1(502:1001),X2(502:1001),'ob'); %plota os elementos da segunda classe
 hold on; %segura o grafico
-
 %construir vetor de entrada com Baes = -1
 X = [-ones(length(X1),1),X1,X2]'; % vetor de entradas X(i) = [Baes,X1(i),X2(i)]
 [atributos,~] = size(X); %retorna o numero de atributos com o Baes
-
 %iniciar W1 aleatoriamente
 n = input('Digite o numero de neuronios da camada oculta: '); %recebe o numero de neuronios da camada oculta
 for i=1:n
@@ -27,9 +23,22 @@ for i=1:n
         W1(i,j) = rand(1); %preenche W1 com numeros aleatorios
     end
 end
-
 %calcular W2
 W2 = (pinv(sigmf(W1*X,[1 1]))'*twomoons(:,3))';
 
 %obter Yout
-Yout = W2*sigmf(W1*X(:,600),[1 1])
+Yout = W2*sigmf(W1*X(:,600),[1 1]);
+
+i=1;
+for a=min(X1):0.1:max(X1)
+    for b=min(X2):0.1:max(X2)
+        c=[-1;a;b];
+        if abs(W2*sigmf(W1*c,[1 1])) < 0.01
+            A(i) = a;
+            B(i) = b;
+            i = i +1;
+            break;
+        end
+    end
+end
+plot(A,B,'-g')
